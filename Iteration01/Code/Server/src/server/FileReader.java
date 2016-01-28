a99d03517393670158a4dc1168b25e9b86ccb6c4
Philip Klostermann <philip.klostermann@gmail.com>
2016-01-27 20:45:56 -0500

    FileWriter and FileReader

--- Iteration01/Code/Server/src/server/FileReader.java
+++ Iteration01/Code/Server/src/server/FileReader.java
@@ -1,5 +1,51 @@
 package server;
 
-public class FileReader {
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class FileReader {
+  private String filename;
+  private BufferedInputStream fileIn;
+    
+  public FileReader(String filename) throws FileNotFoundException {
+    this.setFilename(filename);
+    fileIn = new BufferedInputStream(new FileInputStream(filename));
+  }
+  
+  /**
+   * Reads buffer.length bytes from the file into a buffer.
+   * @param buffer
+   * @return number of bytes read, or -1 on EOF or error.
+   */
+  public int readBlock(byte[] buffer) {
+    try {
+      return fileIn.read(buffer);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return -1;
+    }
+  }
+  
+  public void close() {
+    try {
+      fileIn.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public void setFilename(String filename) {
+    this.filename = filename;
+  }
+
+  @Override
+  public String toString() {
+    return "FileReader [filename=" + filename + ", fileIn=" + fileIn + "]";
+  }
 }
--- Iteration01/Code/Server/src/server/FileWriter.java
+++ Iteration01/Code/Server/src/server/FileWriter.java
@@ -1,5 +1,47 @@
 package server;
 
-public class FileWriter {
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class FileWriter {
+  private String filename;
+  private BufferedOutputStream fileOut;
+  
+  public FileWriter(String filename) throws FileNotFoundException {
+    this.filename = filename;
+    fileOut = new BufferedOutputStream(new FileOutputStream(filename));
+  }
+  
+  public boolean writeBlock(byte[] data) {
+    try {
+      fileOut.write(data, 0, data.length);
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+  
+  public void close() {
+    try {
+      this.fileOut.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public void setFilename(String filename) {
+    this.filename = filename;
+  }
+
+  @Override
+  public String toString() {
+    return "FileWriter [filename=" + filename + ", fileOut=" + fileOut + "]";
+  }
 }
