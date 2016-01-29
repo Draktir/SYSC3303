d683976753e5f63be19c3c72c493773bdd19b9e2
Loktin Wong <loktinw@hotmail.com>
2016-01-29 11:06:18 -0500

    Made changes to Client, file i/o still needs implementation.

--- Iteration01/Code/Client/src/client/Client.java
+++ Iteration01/Code/Client/src/client/Client.java
@@ -1,96 +1,169 @@
+/**
+ * The Client class implements an application that will
+ * send and receive TFTP requests to and from a server
+ * 
+ * @author  Loktin Wong
+ * @version 1.0.0
+ * @since 22-01-2016
+ */
+
+// TODO: add proper documentation to all new functions, and make changes to existing documentation.
+
 package client;
-import java.io.*;
-import java.net.*;
 import packet.*;
+import client.FileReader;
+import client.FileWriter;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Client {
-
-   private DatagramPacket rqstPacket, akg,dataPacket;
-   private DatagramSocket sendReceiveSocket;
-   private DataPacket d;
-   private ReadRequest rrqst;
-   private WriteRequest wrqst;
-   
-
-   public Client()
-   {
-      try {
-         sendReceiveSocket = new DatagramSocket();
-      } catch (SocketException se) {   // Can't create the socket.
-         se.printStackTrace();
-         System.exit(1);
-      }
-   }
-
-   private void sendAndReceive()
-   {
-	   try {
-		wrqst=new WriteRequest(InetAddress.getLocalHost(),68,InetAddress.getLocalHost(),00,"out.dat","ASCII");
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	
+	public static final int SERVER_PORT = 68;
+	public static final int REQUESTS = 11;
+	
+	FileReader fileReader;
+	FileWriter fileWriter;
+	
+	DatagramSocket srSocket;
+	DatagramPacket sendPacket, receivePacket;
+	
+	/**
+	 * Default Client constructor which instantiates a 
+	 * DatagramSocket on an open port on the local machine
+	 */
+	public Client() {
+		try {
+			srSocket = new DatagramSocket();
+		}
+		catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-	   
-	   try {
-		rrqst=new ReadRequest(InetAddress.getLocalHost(),68,InetAddress.getLocalHost(),00,"in.dat","ASCII");
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	
+	/**
+	 * Main method which creates an instance of Client to
+	 * sends alternating read and write TFTP requests to 
+	 * a server on the local machine at the SERVER_PORT port.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Client c = new Client();
+		
+		// Assume we only need to read, and write to a file.
+		
+		c.sendRequest(1, "in.dat", "NeTaScIi");
+		c.sendRequest(2, "out.dat", "oCtEt");
 	}
-	   
-	   try {
-		rqstPacket = new DatagramPacket(wrqst.getPacketData(), wrqst.getPacketData().length,InetAddress.getLocalHost(), 68);
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}//writeRequest
-	   
-	   try {
-		sendReceiveSocket.send(rqstPacket);
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
+	
+	/**
+	 * Sends a TFTP request to a TFTP server based on the type
+	 * of request.
+	 * 
+	 * @param type used to determine what type of request should be sent
+	 * @param file name of the file the client is requesting
+	 * @param mode the mode in which a file is encoded
+	 */	
+	public void sendRequest(int type, String file, String mode) {
+		DatagramPacket requestPacket = null;
+		
+		System.out.println("[SYSTEM] Sending request to server at port " + SERVER_PORT + ".");
+		
+		try {
+			switch (type) {
+			case 1: // RRQ
+				ReadRequest rrq = new ReadRequest(InetAddress.getLocalHost(), SERVER_PORT, InetAddress.getLocalHost(), srSocket.getLocalPort(), file, mode);
+				requestPacket = new DatagramPacket(rrq.getPacketData(), rrq.getPacketData().length, InetAddress.getLocalHost(), SERVER_PORT);
+				srSocket.send(requestPacket);
+				System.out.println("[SYSTEM] Waiting for response from server.");
+				receiveResponse(1); // Receive response from server here, and respond appropriately
+				break;
+			case 2: // WRQ
+				WriteRequest wrq = new WriteRequest(InetAddress.getLocalHost(), SERVER_PORT, InetAddress.getLocalHost(), srSocket.getLocalPort(), file, mode);
+				requestPacket = new DatagramPacket(wrq.getPacketData(), wrq.getPacketData().length, InetAddress.getLocalHost(), SERVER_PORT);
+				srSocket.send(requestPacket);
+				System.out.println("[SYSTEM] Waiting for response from server.");
+				receiveResponse(2); // Receive response from server here, and respond appropriately 
+				break;
+			default:
+				break;
+			}
+		}
+		catch (UnknownHostException e) {
+			// This should not happen in Iteration 1.
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// An error occurred while sending packet out of srSocket.
+			e.printStackTrace();
+		}
+		
+		System.out.println("[SYSTEM] End of request.");
 	}
-	   System.out.println("write request sent.");
-	   byte data[] = new byte[4];
-	   akg = new DatagramPacket(data, data.length);
-	   try {
-		sendReceiveSocket.receive(akg);
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
+	
+	public void receiveResponse(int requestTypeSent) 
+	{
+		DatagramPacket responsePacket = null;
+		
+		byte[] buffer = new byte[516];
+		responsePacket = new DatagramPacket(buffer, buffer.length);
+		
+		try {
+			srSocket.receive(responsePacket);
+		} catch (IOException e) {
+			// An error occurred while receiving a packet from srSocket.
+			e.printStackTrace();
+		}
+		
+		/*
+		 *  Ideally we should check the packet for the appropriate type before handling it,
+		 *  but in this case we know what to expect in response to a particular request.
+		 *  
+		 *  Phil, I'm not too familiar with your packet package, so perhaps you could finish
+		 *  the rest of this up.
+		 *  
+		 *  TODO: Implement the rest of this.
+		 */
+		
+		switch (requestTypeSent) {
+			case 1: // RRQ sent, should receive DATA
+				writeDataToFile(responsePacket);
+				break;
+			case 2: // WRQ sent, should receive ACK
+				sendDataFromFile(responsePacket);
+				break;
+		}
 	}
-	   System.out.println("Akg received: "+akg);
-	   //check the akg
-	   
-	   try {
-		readAndSend();
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	
+	public void writeDataToFile(DatagramPacket packet) {
+		// TODO: implement FileWriter to write to the appropriate file
 	}
-	   System.out.println("data sending finished");
-       sendReceiveSocket.close();
-   }
-
-   private void readAndSend() throws UnknownHostException, IOException{
-	   BufferedInputStream in = new BufferedInputStream(new FileInputStream("in.dat"));
-	   byte[] data = new byte[512];
-	   int n;
-	   while ((n = in.read(data)) != -1) {
-		   d=new DataPacket(InetAddress.getLocalHost(),68,n,data);
-		   dataPacket=new DatagramPacket(d.getPacketData(),d.getPacketData().length);
-		   sendReceiveSocket.send(dataPacket);
-	   }
-	   in.close();
-   }
-
-   public static void main(String args[])
-   {
-      Client c = new Client();
-      c.sendAndReceive();
-   }
+	
+	public void sendDataFromFile(DatagramPacket packet) {
+		// TODO: implement FileReader to read from the appropriate file
+		
+	}
+	
+	/**
+	 * Prints out request contents as a String and in bytes.
+	 * 
+	 * @param buffer
+	 */
+	public void printRequestInformation(byte[] buffer) {
+		String contents = new String(buffer);
+		
+		System.out.println("Request contents: ");
+		System.out.println(contents);
+		
+		System.out.println("Request contents (bytes): ");
+		for (int i = 0; i < buffer.length; i++) {
+			System.out.print(buffer[i] + " ");
+		}
+		System.out.println();
+	}
 }
---
+++ Iteration01/Code/Client/src/client/FileWriter.java
@@ -0,0 +1,76 @@
+package client;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileWriter {
+  private String filename;
+  private BufferedOutputStream fileOut;
+  
+  /**
+   * Constructor, accepts filename of file to be written
+   * 
+   * @param filename
+   * @throws FileNotFoundException
+   */
+  public FileWriter(String filename) throws FileNotFoundException {
+    this.filename = filename;
+    fileOut = new BufferedOutputStream(new FileOutputStream(filename));
+  }
+  
+  /**
+   * Writes a block of data to the file
+   * 
+   * @param data
+   * @return true on success / false on failure
+   */
+  public boolean writeBlock(byte[] data) {
+    try {
+      fileOut.write(data, 0, data.length);
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+  
+  /**
+   * Closes the file output stream
+   * 
+   */
+  public void close() {
+    try {
+      this.fileOut.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Accessor for filename attribute
+   * 
+   * @return filename
+   */
+  public String getFilename() {
+    return filename;
+  }
+
+  /**
+   * Mutator for filename
+   * 
+   * @param filename
+   */
+  public void setFilename(String filename) {
+    this.filename = filename;
+  }
+
+  /**
+   * Generic toString method
+   */
+  @Override
+  public String toString() {
+    return "FileWriter [filename=" + filename + ", fileOut=" + fileOut + "]";
+  }
+}
