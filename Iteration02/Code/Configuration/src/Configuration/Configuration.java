c741e5d2ac018367e673a11c3535a8c8529438c3
Philip Klostermann <philip.klostermann@gmail.com>
2016-02-12 19:56:48 -0500

    set correct port numbers

--- Iteration02/Code/Configuration/src/Configuration/Configuration.java
+++ Iteration02/Code/Configuration/src/Configuration/Configuration.java
@@ -2,7 +2,7 @@ package Configuration;
 
 public class Configuration {
 	// Port the intermediate host is listening on for requests
-  public static final int INTERMEDIATE_PORT = 6800; // grab from intermediatehost 
+  public static final int INTERMEDIATE_PORT = 68; // grab from intermediatehost 
 	// Port the server is listening on for requests
-	public static final int SERVER_PORT = 6900; // grab from intermediatehost
+	public static final int SERVER_PORT = 69; // grab from intermediatehost
 }
